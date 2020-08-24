db24544fb8dba42781db68c661fd8a79afb28070
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-23 18:26:13 -0600

    create new model for crate_products

---
+++ code/api/src/modules/crate_products/model.js
@@ -0,0 +1,16 @@
+// 'use strict'
+//
+// // Product
+// module.exports = function(sequelize, DataTypes) {
+//   return sequelize.define('crate_products', {
+//     crateId: {
+//       type: DataTypes.INTEGER
+//     },
+//     productId: {
+//       type: DataTypes.INTEGER
+//     },
+//     kept: {
+//       type: DataTypes.BOOLEAN
+//     }
+//   })
+// }
