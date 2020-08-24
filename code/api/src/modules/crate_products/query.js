6f42c7b3f16749074ea815f007a8f22c6f402b21
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-23 18:26:55 -0600

    crate product query for creating new crateProducts

---
+++ code/api/src/modules/crate_products/query.js
@@ -0,0 +1,13 @@
+// // Imports
+// import { GraphQLInt, GraphQLList } from 'graphql'
+//
+// // App Imports
+// import CrateProductsType from './types'
+// import { create } from './resolvers'
+// // add updateDelivery method to line 6
+//
+// // crateProducts create
+// export const create = {
+//   type: new GraphQLList(CrateProductsType),
+//   resolve: create
+// }
