a3ae2f3a3d593bc2b12ddeaad507547c6a4350bc
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-23 18:26:30 -0600

    add mutations for crateProducts

---
+++ code/api/src/modules/crate_products/mutations.js
@@ -0,0 +1,27 @@
+// // Imports
+// import { GraphQLString, GraphQLInt } from 'graphql'
+//
+// // App Imports
+// import { CrateProductsType } from './types'
+// import { create } from './resolvers'
+//
+// // CrateProducts create
+//
+// export const crateProductsCreate = {
+//   type: CrateProductsType,
+//   args: {
+//     crateId: {
+//       name: 'crateId',
+//       type: GraphQLInt
+//     },
+//     productId: {
+//       name: 'productId',
+//       type: GraphQLInt
+//     },
+//     kept: {
+//       name: 'kept',
+//       type: GraphQLBoolean
+//     }
+//   },
+//   resolve: create
+// }
