21481c9ca6341ba790a864b6c7b11f2a4373571c
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-24 13:28:15 -0600

    add comments for clarification

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -3,12 +3,17 @@
 // Subscription
 module.exports = function(sequelize, DataTypes) {
   let Subscription = sequelize.define('subscriptions', {
+    // define subscriptions model as 'subscriptions'
     userId: {
       type: DataTypes.INTEGER
+      // set userId to integers
     },
     crateId: {
       type: DataTypes.INTEGER
+      // set crateId to integers
+
     }
+    // setting up the belongs_to relationships for subscriptions to attach to User models and crate models
 
 //  expectedDelivery: {
 //    type: DataTypes.DATE
@@ -17,7 +22,9 @@ module.exports = function(sequelize, DataTypes) {
 
   Subscription.associate = function(models) {
     Subscription.belongsTo(models.User)
+    // setting subscription belongs_to User relationship
     Subscription.belongsTo(models.Crate)
+    // setting subscription belongs_to crate relationship
   }
 
   return Subscription
