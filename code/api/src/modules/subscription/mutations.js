f7c03da9432458f7fe79179cbcd7ceffc9c0fab3
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-24 14:47:48 -0600

    add comments for clarification

--- code/api/src/modules/subscription/mutations.js
+++ code/api/src/modules/subscription/mutations.js
@@ -3,18 +3,24 @@ import { GraphQLInt } from 'graphql'
 
 // App Imports
 import SubscriptionType from './types'
+// import subscription type
 import { create, remove } from './resolvers'
+// import create and remove methods from resolvers file
 
 // Subscription create
 export const subscriptionCreate = {
+  // subscriptionCreate method beginning
   type: SubscriptionType,
+  // set type to SubscriptionType
   args: {
     crateId: {
       name: 'crateId',
       type: GraphQLInt
     }
+    // pass args in for crate_id
   },
   resolve: create
+  // resolver method 'create'
 }
 
 // Subscription update
@@ -38,12 +44,16 @@ export const subscriptionCreate = {
 
 // Subscription remove
 export const subscriptionRemove = {
+  // subscriptionRemove method beginning
   type: SubscriptionType,
+  // set type to SubscriptionType
   args: {
     id: {
       name: 'id',
       type: GraphQLInt
     }
+    // pass args in for subscription_id to destroy the correct object from the database
   },
   resolve: remove
+  // resolver method 'remove'
 }
--- code/api/src/modules/subscription/query.js
+++ code/api/src/modules/subscription/query.js
@@ -3,13 +3,16 @@ import { GraphQLInt, GraphQLList } from 'graphql'
 
 // App Imports
 import SubscriptionType from './types'
+// import subscription type
 import { getAll, getByUser, get } from './resolvers'
+// import resolver methods
 // add updateDelivery method to line 6
 
 // Subscriptions All
 export const subscriptions = {
   type: new GraphQLList(SubscriptionType),
   resolve: getAll
+  // subscriptions query for resolver to getAll subscripitons
 }
 
 // Subscriptions Update Delivery
@@ -20,15 +23,22 @@ export const subscriptions = {
 
 // Subscriptions by user
 export const subscriptionsByUser = {
+  // subscriptionsByUser method creation
   type: new GraphQLList(SubscriptionType),
+  // set type to SubscriptionType
   resolve: getByUser
+  // resolver emthod getByUser
 }
 
 // Subscription By id
 export const subscription = {
+  // subscription method creation
   type: SubscriptionType,
+  // set type to SubscriptionType
   args: {
     id: { type: GraphQLInt }
   },
+  // pass in Subscription Id
   resolve: get
+  //resovles get method for resolver
 }
