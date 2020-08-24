c21bb6cc016f931848b09357be18912a33a08c8d
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-24 13:17:20 -0600

    add in annotations from group

--- code/api/src/modules/crate_products/model.js
+++ code/api/src/modules/crate_products/model.js
@@ -3,14 +3,18 @@
 // // Product
 // module.exports = function(sequelize, DataTypes) {
 //   return sequelize.define('crate_products', {
+    // name crate_products model
 //     crateId: {
 //       type: DataTypes.INTEGER
+        // set datatype to integer
 //     },
 //     productId: {
 //       type: DataTypes.INTEGER
+        // set datatype to integer
 //     },
 //     kept: {
 //       type: DataTypes.BOOLEAN
+        // set datatype to integer
 //     }
 //   })
 // }
