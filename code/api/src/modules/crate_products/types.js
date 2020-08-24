df9a1763e89e7b54b182c72ec8692191abfd4ef2
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-23 18:27:17 -0600

    types creation

---
+++ code/api/src/modules/crate_products/types.js
@@ -0,0 +1,21 @@
+// // Imports
+// import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql'
+//
+// // App Imports
+// import { CrateType } from '../crate/types'
+// import ProductType from '../product/types'
+//
+// // Subscription type
+// const CrateProductsType = new GraphQLObjectType({
+//   name: 'crate_products',
+//   description: 'Crate Products Type',
+//
+//   fields: () => ({
+//     id: { type: GraphQLInt },
+//     product: { type: ProductType },
+//     crate: { type: CrateType },
+//     kept: { type: GraphQLBoolean }
+//   })
+// })
+//
+// export default CrateProductsType
