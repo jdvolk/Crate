e853a5e6b81a34064c5c156655b6585be865e1da
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-24 14:59:05 -0600

    add comment for clarification

--- code/api/src/modules/subscription/types.js
+++ code/api/src/modules/subscription/types.js
@@ -1,14 +1,18 @@
 // Imports
 import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql'
+//import datatypes from GraphQl
 
 // App Imports
 import { UserType } from '../user/types'
 import CrateType from '../crate/types'
+//import user and crate types
 
 // Subscription type
 const SubscriptionType = new GraphQLObjectType({
+  //set variable name to SubscriptionType
   name: 'subscription',
   description: 'Subscription Type',
+  // name attributes for new GraphQl object
 
   fields: () => ({
     id: { type: GraphQLInt },
@@ -16,7 +20,10 @@ const SubscriptionType = new GraphQLObjectType({
     crate: { type: CrateType },
     createdAt: { type: GraphQLString },
     updatedAt: { type: GraphQLString }
+    // pass in fields of actual subscription Object attributes
+    // will need to pass in expecte3dDelivery as well
   })
 })
 
 export default SubscriptionType
+//export variable named on line 11
