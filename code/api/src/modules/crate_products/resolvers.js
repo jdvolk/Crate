45c6861c68f65cc0f71e1b5ad9821327c2d5f830
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-23 18:27:08 -0600

    create resolver for new createProduct

---
+++ code/api/src/modules/crate_products/resolvers.js
@@ -0,0 +1,12 @@
+// // App Imports
+// import models from '../../setup/models'
+//
+//
+// // Create subscription
+// export async function create(parentValue, { crateId }, { productId }) {
+//   return await models.CrateProducts.create({
+//     crateId,
+//     productId,
+//     kept: false
+//   })
+// }
