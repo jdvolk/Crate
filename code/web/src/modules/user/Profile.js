84d14f2ea6d13fa245f3a76478e624cbd5a23dbc
Liliana Weimer <lilianaweimer11@gmail.com>
2020-08-25 17:04:17 -0600

    Add basic user info display and convert Profile to class component

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -16,8 +16,15 @@ import userRoutes from '../../setup/routes/user'
 import { logout } from './api/actions'
 
 // Component
-const Profile = (props) => (
-  <div>
+class Profile extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
     {/* SEO */}
     <Helmet>
       <title>My Profile - Crate</title>
@@ -32,19 +39,30 @@ const Profile = (props) => (
 
     <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
+        <H4 style={{ marginBottom: '0.5em' }}>{this.props.user.details.name}</H4>
 
-        <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>
+        <img src='' alt='your profile image'/><br />
+        <Button theme="primary" style={{ margin: '0.5em' }}>Update Image</Button>
+
+
+        <p style={{ color: grey2, marginBottom: '2em' }}>{this.props.user.details.email}</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>{this.props.user.details.shippingAddress} address</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>{this.props.user.details.city}city, {this.props.user.details.state}state {this.props.user.details.zip}zip</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>{this.props.user.details.description}description</p>
+        <Button theme="primary" style={{ marginRight: '1em' }} onClick={this.toggleModal}>Update Profile</Button>
 
         <Link to={userRoutes.subscriptions.path}>
           <Button theme="primary">Subscriptions</Button>
         </Link>
 
-        <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
+        <Button theme="secondary" onClick={this.props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
       </GridCell>
     </Grid>
   </div>
-)
+    )
+  }
+  
+}
 
 // Component Properties
 Profile.propTypes = {
