e5910dfb463c33f141a23d4d2c650ef43c9f6350
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-27 13:19:45 -0600

    additions to query test

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -1,4 +1,32 @@
+import request from 'supertest'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from '../../setup/schema'
+
 describe('user_queries', () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false,
+      })
+    )
+  })
+
+  it('returns all users', async () => {
+    const response = await request(server)
+    .get('/')
+    .send({ query: '{ users { email name } }'})
+    .expect(200)
+
+    console.log(response.body)
+  })
+
   it('is_true', () => {
     expect(true).toBe(true)
   })
