07de5db736cc2e4adecce5230bd2117cd4ae0054
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-29 13:52:56 -0600

    add image seeds for admin and default Users

--- code/api/src/seeders/1-user.js
+++ code/api/src/seeders/1-user.js
@@ -17,6 +17,7 @@ module.exports = {
         zip: "75287",
         shipping_address: "1042 N Flagstaff Ave",
         role: params.user.roles.admin,
+        image: 'https://i.pinimg.com/originals/3d/e9/1a/3de91acba161633edafac2df468c3b6f.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -30,6 +31,7 @@ module.exports = {
         zip: "32541",
         shipping_address: "1042 N Beach St",
         role: params.user.roles.user,
+        image: 'https://i.pinimg.com/originals/20/0e/47/200e4762b3f505c959379bd56f0c06f4.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       }
